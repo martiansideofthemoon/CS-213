aa8bcc09635a94e87de6e9e030bc85fe37d8dd92
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-02-15 18:35:18 +0530

    Using unordered maps now

--- Assignment 3/Q1/count_uniq.cpp
+++ Assignment 3/Q1/count_uniq.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
-#include <unordered_set>
-#include "mfile.hpp"
+#include <vector>
+#include <unordered_map>
 #include <functional>
+#include "mfile.hpp"
 using namespace std;
 int main() {
 	string file;
@@ -13,15 +14,38 @@ int main() {
 	int unique = 0;
 	MFile f;
 	f.init(file, n);
-	unordered_set<long> lines;
+	// The key is the hash of the string
+	// The value is a vector of line numbers having this hash
+	unordered_map< int, vector<int> > lines;
 	string s = f.readNext();
-	long k = str_hash(s);
+	int line_no = -1;
 	while(s.length() > 0) {
-		k = str_hash(s);
-		unordered_set<long>::iterator a = lines.find(k);
-		if (a == lines.end()) {
+		line_no++;
+		int k = str_hash(s);
+		unordered_map< int, vector<int> >::iterator a1 = lines.find(k);
+		if (a1 == lines.end()) {
+			// Element hash doesn't exist! It's definitely unique!
 			unique++;
-			lines.insert(k);
+			vector<int> a;
+			a.push_back(line_no);
+			lines.insert(pair<int, vector<int> >(k, a));
+		} else {
+			// Investigate strings of this hash
+			vector<int> a = a1->second;
+			bool string_found = false;
+			vector<string> matches = f.readLines(a);
+			vector<string>::iterator it = matches.begin();
+			while (it != matches.end()) {
+				if (s == *it) {
+					string_found = true;
+					break;
+				}
+				it++;
+			}
+			if (!string_found) {
+				unique++;
+				(a1->second).push_back(line_no);
+			}
 		}
 		s = f.readNext();
 	}
