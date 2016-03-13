eb4b9d121c4778a0e9b975302b532931108d37b0
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-03-13 15:21:11 +0530

    Adding working question 1 and python input generator

---
+++ Assignment 4/Q1/main.cpp
@@ -0,0 +1,49 @@
+#include "MinMaxHeap.hpp"
+#include <iostream>
+using namespace std;
+
+template<class T>
+class subPredicate: public Predicate<T> {
+	public:
+		bool toDelete(T elem) {
+			if (elem % 2 == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+};
+int main() {
+	MinMaxHeap<int> a;
+	char command;
+	cin >> command;
+	while (command != 'q') {
+		switch(command) {
+			case 'p':
+				a.printVector();
+				break;
+			case 'i':
+				int b;
+				cin >> b;
+				a.insert(b);
+				break;
+			case 'g':
+				cout << a.getMax() << endl;
+				break;
+			case 'h':
+				cout << a.getMin() << endl;
+				break;
+			case 'd':
+				a.deleteMax();
+				break;
+			case 'e':
+				a.deleteMin();
+				break;
+			case 'f':
+				subPredicate<int> c;
+				a.deleteElems(&c);
+				break;
+		}
+		cin >> command;
+	}
+}
